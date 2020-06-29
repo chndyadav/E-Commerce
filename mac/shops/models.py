e9f48daf8dbafef4bb9a1768199e1e42aad28d87
Chandrakant <chndyadav@gmail.com>
2020-06-29 17:02:03 +0530

    na

--- mac/shops/models.py
+++ mac/shops/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    product_id = models.AutoField
+    product_name = models.CharField(max_length=50)
+    category = models.CharField(max_length=50, default="")
+    subcategory = models.CharField(max_length=50, default='')
+    price = models.IntegerField(default=0)
+    description = models.CharField(max_length=300)
+    public_date = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField(upload_to="shops/images", default='')
+
+    def __str__(self):
+        return self.product_name
--- mac/shops/views.py
+++ mac/shops/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Product
+from math import ceil
 
 
 def index(request):
-    return render(request, "shops/index.html")
+    products = Product.objects.all()
+    n = len(products)
+    nslide = n // 4 + ceil((n / 4) - (n // 4))
+    params = {"no_of_slides": nslide, "range": range(1, nslide), "product": products}
+    return render(request, "shops/index.html", params)
 
